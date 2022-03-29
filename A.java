584a7a35e9976fd20467be55030ea55bedd50e7d
nareshkumar1997 <nauticalnaresh@gmail.com>
2022-03-29 09:08:12 +0530

    Test1

---
+++ A.java
@@ -0,0 +1,20 @@
+package org.test1;
+
+public class A extends B{
+
+	private void empInfo() {
+		System.out.println("Employee Info");
+	}
+public static void main(String[] args) {
+	
+	A a=new A();
+	a.empInfo();a.comInfo();a.clientInfo();
+	
+	
+}
+
+
+
+
+
+}
---
+++ C.java
@@ -0,0 +1,13 @@
+package org.test1;
+
+public interface C {
+    void comInfo(); 
+	static void comName() {
+	System.out.println("Naresh");	
+	}	
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
