c2bd58b15eb1bc425a871fdac9cc5e6670d40141
nareshkumar1997 <nauticalnaresh@gmail.com>
2022-03-29 21:37:04 +0530

    updated A

--- A.java
+++ A.java
@@ -5,6 +5,11 @@ public class A extends B{
 	private void empInfo() {
 		System.out.println("Employee Info");
 	}
+	private void empName() {
+		System.out.println("Naresh");
+	}
+	
+	
 public static void main(String[] args) {
 	
 	A a=new A();
