b98f0aea560288b2fdcca284216d04664257c8af
nareshkumar1997 <nauticalnaresh@gmail.com>
2022-03-29 09:14:47 +0530

    New code

---
+++ Test 1/B.java
@@ -0,0 +1,20 @@
+package org.test1;
+
+public class B {
+public void comInfo() {
+	System.out.println("Company Info");
+}
+
+public void clientInfo() {
+	System.out.println("Updated");
+
+}
+
+public static void main(String[] args) {
+	B b = new B();
+b.comInfo();b.clientInfo();
+
+
+}
+
+}
